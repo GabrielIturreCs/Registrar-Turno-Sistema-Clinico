1e318c6e15f0d1ecdb6678519ce02579aa4414fc
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-06-22 18:08:35 -0300

    subo el routing para los destinos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ReservarComponent } from './components/reservar/reservar.component';
+import { TurnosComponent } from './components/turnos/turnos.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/layouts/home/home.component';
+import { AgendaComponent } from './components/agenda/agenda.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EstadisticaComponent } from './components/estadistica/estadistica.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 
-export const routes: Routes = [];
+import { ReprogramarComponent } from './components/reprogramar/reprogramar.component';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard()] },
+  { path: 'misTurnos', component: TurnosComponent, canActivate: [authGuard('paciente')] },
+  { path: 'reservarTurno', component: ReservarComponent, canActivate: [authGuard()] },
+  { path: 'agenda', component: AgendaComponent, canActivate: [authGuard('dentista')] },
+  { path: 'reprogramar', component: ReprogramarComponent, canActivate: [authGuard('dentista')] },
+  { path: 'estadisticas', component: EstadisticaComponent, canActivate: [authGuard()] },
+  { path: 'admin', component: AdministradorComponent, canActivate: [authGuard('administrador')] },
+];
