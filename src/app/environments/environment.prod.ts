7cd07b3be52ab39d8e68d27d2c48e35b5dae86c7
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-07-04 02:53:43 -0300

    Actualizar la URL del backend en environment.prod.ts para apuntar al servidor correcto.

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-url.onrender.com/api', // Cambia esto por tu URL del backend en Render
+  apiUrl: 'https://registro-de-turno-sistema-odontologico.onrender.com/api', // URL del backend en Render
   googleClientId: "289556059590-492hb4ibk081c42f3ldkjk62ohbmoppm.apps.googleusercontent.com",
   someFeatureToggle: true
 };
