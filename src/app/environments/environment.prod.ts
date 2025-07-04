4b40e36eaeaded762e2688d9089fa4d2b5ba083a
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-07-04 02:22:49 -0300

    Agregar configuración de producción y redirecciones para el despliegue. Se incluye el archivo _redirects y se actualiza el entorno de producción en environment.prod.ts.

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend-url.onrender.com/api', // Cambia esto por tu URL del backend en Render
+  googleClientId: "289556059590-492hb4ibk081c42f3ldkjk62ohbmoppm.apps.googleusercontent.com",
+  someFeatureToggle: true
+};
