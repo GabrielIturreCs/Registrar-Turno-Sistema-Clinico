8b3c0a7265d30195ff81ad9390655492fb6c002a
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-07-06 00:40:18 -0300

    Update API URL in production environment configuration to the correct backend address

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-develop-nu3j.onrender.com/api', // Backend en Render
+  apiUrl: 'https://registro-de-turno-sistema-odontologico.onrender.com/api', // ✅ URL correcta del backend
   googleClientId: "585856608795-5tnd7elkktntqm7b95g4livkppcjgp1u.apps.googleusercontent.com",
   someFeatureToggle: true
 };
