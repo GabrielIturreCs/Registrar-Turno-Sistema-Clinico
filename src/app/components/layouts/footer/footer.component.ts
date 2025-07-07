9b4cd80392e6f74a6ebb9b7d43466a2ccbda6350
Ivo <ivothaielvicencio@gmail.com>
2025-07-06 21:06:06 -0300

    mejora sobre el pie de pagina

--- src/app/components/layouts/footer/footer.component.ts
+++ src/app/components/layouts/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  showBackToTop = false;
 
+  ngOnInit(): void {
+    this.checkScrollPosition();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScrollPosition();
+  }
+
+  private checkScrollPosition(): void {
+    this.showBackToTop = window.pageYOffset > 300;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
